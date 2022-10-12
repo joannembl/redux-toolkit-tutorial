e0e060930bac84399195636d863ee0cc4d5dd476
joannembl <jmbliberato@gmail.com>
2022-10-12 14:05:32 -0700

    everything before modal

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,22 @@
+import NavBar from "./components/NavBar";
+import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useEffect } from "react";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      <NavBar />
+      <CartContainer />
+    </main>
+  )
 }
 export default App;
